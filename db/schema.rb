3343c0e54c4d5902869556b167691b6a5f57e62a
HMTanzeel <mtanzeel6363@gmail.com>
2021-12-29 12:30:33 +0500

    has_many through relation in physician and patient via third model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_135846) do
+ActiveRecord::Schema.define(version: 2021_12_29_072710) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "physician_id", null: false
+    t.integer "patient_id", null: false
+    t.datetime "appointment_date", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["physician_id"], name: "index_appointments_on_physician_id"
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -26,5 +36,19 @@ ActiveRecord::Schema.define(version: 2021_12_28_135846) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "physicians", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "appointments", "physicians"
   add_foreign_key "books", "authors"
 end
