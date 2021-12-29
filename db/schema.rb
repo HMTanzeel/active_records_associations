962b9a06c940218407d2747d5fbad11967fde55c
HMTanzeel <mtanzeel6363@gmail.com>
2021-12-29 15:08:37 +0500

    has and belongs to many relation with assembly & part

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_080447) do
+ActiveRecord::Schema.define(version: 2021_12_29_100310) do
 
   create_table "account_histories", force: :cascade do |t|
     t.text "name"
@@ -38,6 +38,19 @@ ActiveRecord::Schema.define(version: 2021_12_29_080447) do
     t.index ["physician_id"], name: "index_appointments_on_physician_id"
   end
 
+  create_table "assemblies", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "assemblies_parts", id: false, force: :cascade do |t|
+    t.integer "assembly_id"
+    t.integer "part_id"
+    t.index ["assembly_id"], name: "index_assemblies_parts_on_assembly_id"
+    t.index ["part_id"], name: "index_assemblies_parts_on_part_id"
+  end
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -50,6 +63,12 @@ ActiveRecord::Schema.define(version: 2021_12_29_080447) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "parts", force: :cascade do |t|
+    t.string "part_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "patients", force: :cascade do |t|
